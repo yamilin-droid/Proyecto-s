e916556bc62b56f107dfd2a3dcfb51291400f83a
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-04 19:56:16 -0600

    Add files via upload

---
+++ Arreglo abstracto python.py
@@ -0,0 +1,11 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+# Lista (arreglo) de Personas
+personas = [
+    Persona("Ana", 20),
+    Persona("Luis", 25),
+    Persona("Maria", 22)
+]
