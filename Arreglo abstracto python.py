9ec2b14b7b21942762f48dd73937fdef30447443
Yamill Hdez Glum <yamill29.7.6@gmail.com>
2025-09-04 19:58:45 -0600

    Update Arreglo abstracto python.py

--- Arreglo abstracto python.py
+++ Arreglo abstracto python.py
@@ -3,9 +3,9 @@ class Persona:
         self.nombre = nombre
         self.edad = edad
 
-# Lista (arreglo) de Personas
 personas = [
     Persona("Ana", 20),
     Persona("Luis", 25),
     Persona("Maria", 22)
+
 ]
